9152b416d11ee19f50cfe19b17e282df775a90d9
Zach Newton <znewton@iastate.edu>
2017-02-10 15:01:00 -0600

    Day 2 ~ 50% done

--- scripts/day2.js
+++ scripts/day2.js
@@ -1,19 +1,22 @@
 
 var unitPrice = 320;
-var quantity = 1;
+var quantity = 3;
 var totalPrice = quantity * unitPrice;
 
 function changeQuantity(amount) {
-    quantity += amount;
-    totalPrice = quantity * unitPrice;
-    document.getElementById('total-price').innerHTML = parseFloat(Math.round(totalPrice * 100) / 100).toFixed(2);
-    document.getElementById('quantity').innerHTML = ("0" + quantity).slice(-2);
+    if(quantity > 0 || amount > 0) {
+        quantity += amount;
+        totalPrice = quantity * unitPrice;
+        document.getElementById('total-price').innerHTML = parseFloat(Math.round(totalPrice * 100) / 100).toFixed(2);
+        document.getElementById('quantity').innerHTML = ("0" + quantity).slice(-2);
+    }
 }
 function init(){
-    console.log('unit');
     document.getElementById('unit-price').innerHTML = parseFloat(Math.round(unitPrice * 100) / 100).toFixed(2);
-    console.log('total');
     document.getElementById('total-price').innerHTML = parseFloat(Math.round(totalPrice * 100) / 100).toFixed(2);
+    document.getElementById('quantity').innerHTML = ("0" + quantity).slice(-2);
+
+    document.getElementById('increase-quantity').onclick = function(){return changeQuantity(1)};
+    document.getElementById('decrease-quantity').onclick = function(){return changeQuantity(-1)};
 }
-// init();
-console.log(document.body);
+init();
